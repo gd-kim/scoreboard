7bf83c2f1656cc92eb37604ed6db52504c2cde83
gdkim <oyammo@hanmail.net>
2020-02-29 11:05:22 +0900

    20200229 1

--- src/App.js
+++ src/App.js
@@ -1,63 +1,9 @@
 import React from 'react';
 import './App.css';
 import {Header} from './components/Header';
+import {Player} from './components/Player';
 
-const Player = (props) => {
-  return (
-    <div className="player">
-            <span className="player-name">
-              <button className="remove-player" onClick={()=>props.removePlayer(props.id)}> x </button>
-              {props.name}</span>
-      <Counter score={props.score}/>
-    </div>
-  );
-}
 
-class Counter extends React.Component {
-  //클래스 바로 아래에 선언되는 변수는 속성이 된다.
-  constructor() {
-    super();
-    this.state = {
-      score: 0
-    }
-
-    this.handleChange.bind(this);
-  }
-  handleChange(delta){
-    this.setState(prevState=>{
-      return {score:prevState.score+delta } //비동기로 동작
-    });
-
-  }
-  /*
-  plusScore = () => {
-    // 값을 변경하고 UI도 렌더링할려면 반드시 setState로 변경
-    this.setState(prevState=>{
-      return {score:prevState.score+1 } //비동기로 동작
-    });
-  }
-  minusScore= ()=>{
-    // 값을 변경하고 UI도 렌더링할려면 반드시 setState로 변경
-    this.setState(
-      {
-        score: this.state.score-1
-      }
-
-    );
-  }
-
-   */
-  //이벤트 핸들러 오른쪽은 반드시 함수 선언문이 와야 한다.
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={()=>this.handleChange(-1)}> -</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={()=>this.handleChange(+1)}> +</button>
-      </div>
-    );
-  }
-}
 
 class App extends React.Component {
   state = {
@@ -75,7 +21,7 @@ class App extends React.Component {
       const players = prevState.players.filter(player=>player.id !== id);
       return {players}; //*** es6 short hand property : 키와 값이 같으면 한쪽을 생략
     })
-  }
+  };
 
   render() {
     return (
---
+++ src/components/Counter.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export class Counter extends React.Component {
+  //클래스 바로 아래에 선언되는 변수는 속성이 된다.
+  constructor() {
+    super();
+    this.state = {
+      score: 0
+    };
+
+    this.handleChange.bind(this);
+  }
+  handleChange(delta){
+    this.setState(prevState=>{
+      return {score:prevState.score+delta } //비동기로 동작
+    });
+
+  }
+  /*
+  plusScore = () => {
+    // 값을 변경하고 UI도 렌더링할려면 반드시 setState로 변경
+    this.setState(prevState=>{
+      return {score:prevState.score+1 } //비동기로 동작
+    });
+  }
+  minusScore= ()=>{
+    // 값을 변경하고 UI도 렌더링할려면 반드시 setState로 변경
+    this.setState(
+      {
+        score: this.state.score-1
+      }
+
+    );
+  }
+
+   */
+  //이벤트 핸들러 오른쪽은 반드시 함수 선언문이 와야 한다.
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={()=>this.handleChange(-1)}> -</button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={()=>this.handleChange(+1)}> +</button>
+      </div>
+    );
+  }
+}
